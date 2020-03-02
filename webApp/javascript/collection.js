4fecba9c6f0f9a95645e41c270caede1a4651f2e
mokeeemokeee <moke.0922.moke@gmail.com>
2020-03-03 01:27:29 +0900

    09 add

---
+++ webApp/javascript/collection.js
@@ -0,0 +1,7 @@
+var classes = ['A組', 'B組', 'C組', 'D組']
+
+for (var grade = 1; grade < 4; grade++) {
+  for (var i = 0; i < classes.length; i++) {
+    document.write('<p>' + grade + '年' + classes[i] + '</p>')
+  }
+}
