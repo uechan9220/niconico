e86174adb62a68c95996a776ea0d5bcc04de3ee4
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-03-11 15:45:26 +0900

    fix

--- serverSide/workspace/node-js-http/index.js
+++ serverSide/workspace/node-js-http/index.js
@@ -2,12 +2,14 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset-8'
-  });
-  res.write(req.headers['user-agent'])
+    'Content-Type': 'text/html; charset=utf-8'
+  })
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>'
+  )
   res.end()
 })
-const post = 8000
+const port = 8000
 server.listen(port, () => {
-  console.log('Listening on' + port)
+  console.log('Listening on ' + port)
 })
