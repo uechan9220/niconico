1da50171ca7f16375e436bddb6c730d8024f6432
MokeeeMokeee <moke.0922.moke@gmail.com>
2020-03-11 17:28:53 +0900

    node-js-http-3017 add

---
+++ serverSide/workspace/node-js-http-3017/index.js
@@ -0,0 +1,86 @@
+'use strict'
+const http = require('http')
+const jade = require('jade')
+const auth = require('http-auth')
+
+const basic = auth.basic(
+  { realm: 'Enquetes Area.' },
+  (username, password, callback) => {
+    callback(username === 'guest' && password === 'password')
+  }
+)
+const server = http
+  .createServer(basic, (req, res) => {
+    console.info('Requested by ' + req.connection.remoteAddress)
+    if (req.url === '/logout') {
+      res.writeHead(401, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      })
+      res.end('ログアウトしました')
+      return
+    }
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    switch (req.method) {
+      case 'GET':
+        if (req.url === '/enquetes/yaki-shabu') {
+          res.write(
+            jade.renderFile('./form.jade', {
+              path: req.url,
+              firstItem: '焼き肉',
+              secondItem: 'しゃぶしゃぶ'
+            })
+          )
+        } else if (req.url === '/enquetes/rice-bread') {
+          res.write(
+            jade.renderFile('./form.jade', {
+              path: req.url,
+              firstItem: 'ごはん',
+              secondItem: 'パン'
+            })
+          )
+        } else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(
+            jade.renderFile('./form.jade', {
+              path: req.url,
+              firstItem: '寿司',
+              secondItem: 'ピザ'
+            })
+          )
+        }
+        res.end()
+        break
+      case 'POST':
+        let body = []
+        req
+          .on('data', chunk => {
+            body.push(chunk)
+          })
+          .on('end', () => {
+            body = Buffer.concat(body).toString()
+            const decoded = decodeURIComponent(body)
+            console.info('投稿: ' + decoded)
+            res.write(
+              '<!DOCTYPE html><html lang="jp"><head><meta charset="utf-8"></head><body><h1>' +
+                decoded +
+                'が投稿されました</h1></body></html>'
+            )
+            res.end()
+          })
+        break
+      default:
+        break
+    }
+  })
+  .on('error', e => {
+    console.error('Server Error', e)
+  })
+  .on('clientError', e => {
+    console.error('Client Error', e)
+  })
+const port = process.env.PORT || 8000
+server.listen(port, () => {
+  console.info('Listening on ' + port)
+})
